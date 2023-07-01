21fc4f684683e6cf4f24102c395dca7c6d376354
Karapet Sahatchian <k.sahatchian@codeex.io>
2023-07-02 03:39:35 +0400

    Fix for Bishop availableMoves

--- classes/special-pieces/Bishop.cpp
+++ classes/special-pieces/Bishop.cpp
@@ -19,8 +19,56 @@ Bishop::Bishop(
 }
 
 std::pair<Coords*, unsigned int> Bishop::getAvailableMoves() const {
-	// TODO implement
-	return std::pair(nullptr, 0);
+	Coords* availableMoves = new Coords[14];
+	unsigned int availableMovesCount = 0;
+	Coords newCoords;
+	const short boardHeight = this->board.getHeight();
+	const short boardWidth = this->board.getWidth();
+	bool upRight = true;
+	bool upLeft = true;
+	bool downRight = true;
+	bool downLeft = true;
+	for (char i = 1; i < 8; i++) {
+		newCoords = { this->coords.y + i, this->coords.x + i };
+		if (upRight && 
+			newCoords.x >= 0 && newCoords.x < boardWidth && 
+			newCoords.y >= 0 && newCoords.y < boardHeight && 
+			this->canMove(newCoords)) {
+			availableMoves[availableMovesCount++] = newCoords;
+		} else {
+			upRight = false;
+		}
+		newCoords = { this->coords.y - i, this->coords.x + i };
+		if (downRight && 
+			newCoords.x >= 0 && newCoords.x < boardWidth &&
+			newCoords.y >= 0 && newCoords.y < boardHeight && 
+			this->canMove(newCoords)) {
+			availableMoves[availableMovesCount++] = newCoords;
+		} else {
+			downRight = false;
+		}
+		newCoords = { this->coords.y - i, this->coords.x - i };
+		if (downLeft &&
+			newCoords.x >= 0 && newCoords.x < boardWidth &&
+			newCoords.y >= 0 && newCoords.y < boardHeight &&
+			this->canMove(newCoords)) {
+			availableMoves[availableMovesCount++] = newCoords;
+		} else {
+			downLeft = false;
+		}
+		newCoords = { this->coords.y + i, this->coords.x - i };
+		if (upLeft &&
+			newCoords.x >= 0 && newCoords.x < boardWidth &&
+			newCoords.y >= 0 && newCoords.y < boardHeight &&
+			this->canMove(newCoords)) {
+			availableMoves[availableMovesCount++] = newCoords;
+		}
+		else {
+			upLeft = false;
+		}
+	}
+
+	return std::pair(availableMoves, availableMovesCount);
 }
 
 bool Bishop::canMove(Coords coords) const {
@@ -30,12 +78,12 @@ bool Bishop::canMove(Coords coords) const {
 		if (piece && !this->isSameColor(piece) || !piece) {
 			if (delta.x && delta.y) {
 				const char xStart = delta.x < 0 ? coords.x : this->coords.x;
+				const char yStart = delta.x < 0 ? coords.y : this->coords.y;
 				const char xEnd = delta.x >= 0 ? coords.x : this->coords.x;
-				const char yStart = delta.y >= 0 ? coords.y : this->coords.y;
-				const char yEnd = delta.y < 0 ? coords.y : this->coords.y;
-				for (char i = xStart + 1, j = yStart + 1; i < xEnd && j < yEnd; i++, j++) {
-					Piece* p = this->board.pieceAt({ i, j });
-					if (p) {
+				const char yEnd = delta.x >= 0 ? coords.y : this->coords.y;
+				std::int8_t yCoef = yEnd < yStart ? -1 : 1;
+				for (char i = xStart + 1, j = yStart + yCoef; i < xEnd; i++, j += yCoef) {
+					if (this->board.pieceAt({ i, j })) {
 						return false;
 					}
 				}
